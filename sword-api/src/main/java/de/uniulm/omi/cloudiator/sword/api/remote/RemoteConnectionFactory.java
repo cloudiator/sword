eac48cd5f900956bb2da2c1a4da4676058feb4fe
Daniel Baur <daniel.baur@uni-ulm.de>
2015-08-18 18:11:03 +0200

    fixed documentation

--- sword-api/src/main/java/de/uniulm/omi/cloudiator/sword/api/remote/RemoteConnectionFactory.java
+++ sword-api/src/main/java/de/uniulm/omi/cloudiator/sword/api/remote/RemoteConnectionFactory.java
@@ -9,12 +9,13 @@ import de.uniulm.omi.cloudiator.sword.api.domain.OSFamily;
 public interface RemoteConnectionFactory {
 
     /**
-     * Creates a RemoteConnection to a remote host
-     * @param remoteAddress
-     * @param osFamily
-     * @param loginCredential
-     * @param port the specific port for opening a RemoteConnection
-     * @return
+     * Creates a RemoteConnection to a remote host.
+     *
+     * @param remoteAddress   the address of the remote machine
+     * @param osFamily        the operating system family of the remote
+     * @param loginCredential the login credentials for login.
+     * @param port            the specific port for opening a RemoteConnection.
+     * @return a connection to the remote machine.
      */
     RemoteConnection createRemoteConnection(String remoteAddress, OSFamily osFamily,
         LoginCredential loginCredential, int port);
