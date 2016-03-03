27f1a054a8be3e159f3dba65f907580fa80ef8f9
Daniel Baur <daniel.baur@uni-ulm.de>
2016-03-03 13:17:12 +0100

    PS-150 virtual machine template now supports a name

--- core/src/test/java/de/uniulm/omi/cloudiator/sword/core/domain/VirtualMachineTemplateImplTest.java
+++ core/src/test/java/de/uniulm/omi/cloudiator/sword/core/domain/VirtualMachineTemplateImplTest.java
@@ -20,8 +20,6 @@ package de.uniulm.omi.cloudiator.sword.core.domain;
 
 import de.uniulm.omi.cloudiator.sword.api.domain.TemplateOptions;
 import de.uniulm.omi.cloudiator.sword.api.domain.VirtualMachineTemplate;
-import de.uniulm.omi.cloudiator.sword.core.domain.TemplateOptionsBuilder;
-import de.uniulm.omi.cloudiator.sword.core.domain.VirtualMachineTemplateBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -36,6 +34,7 @@ public class VirtualMachineTemplateImplTest {
     String imageId = "imageId";
     String locationId = "locationId";
     String hardwareId = "hardwareId";
+    String testName = "name";
     TemplateOptions templateOptions =
         TemplateOptionsBuilder.newBuilder().keyPairName("keyPairName").addOption("key", "value")
             .build();
@@ -46,14 +45,20 @@ public class VirtualMachineTemplateImplTest {
 
     @Before public void setUp() throws Exception {
         validVirtualMachineTemplateBuilder =
-            VirtualMachineTemplateBuilder.newBuilder().hardwareFlavor(hardwareId).image(imageId)
-                .location(locationId).templateOptions(templateOptions);
+            VirtualMachineTemplateBuilder.newBuilder().name(testName).hardwareFlavor(hardwareId)
+                .image(imageId).location(locationId).templateOptions(templateOptions);
         validVirtualMachineTemplate = validVirtualMachineTemplateBuilder.build();
     }
 
     @Test(expected = NullPointerException.class) public void testConstructorDisallowsNullImageId()
         throws Exception {
         validVirtualMachineTemplateBuilder.image(null).build();
+    }
+
+    @Test public void testConstructorAllowsNullName() throws Exception {
+        VirtualMachineTemplate nullNameTemplate =
+            validVirtualMachineTemplateBuilder.name(null).build();
+        assertThat(nullNameTemplate.name().isPresent(), equalTo(false));
     }
 
     @Test(expected = NullPointerException.class)
@@ -79,6 +84,10 @@ public class VirtualMachineTemplateImplTest {
     @Test(expected = IllegalArgumentException.class)
     public void testConstructorDisallowsEmptyHardwareId() throws Exception {
         validVirtualMachineTemplateBuilder.hardwareFlavor("").build();
+    }
+
+    @Test public void testName() throws Exception {
+        assertThat(validVirtualMachineTemplate.name().get(), equalTo(testName));
     }
 
     @Test public void testImageId() throws Exception {
