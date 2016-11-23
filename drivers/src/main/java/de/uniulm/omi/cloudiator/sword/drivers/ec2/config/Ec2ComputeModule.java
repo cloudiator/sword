96d8afc5b331aa8441d2691156eca86b7188e1b5
Daniel Baur <daniel.baur@uni-ulm.de>
2016-11-23 17:56:52 +0200

    Ec2CreateVirtualMachineStrategy implemented. Ensures that a public ip address is present in started VM

--- drivers/src/main/java/de/uniulm/omi/cloudiator/sword/drivers/ec2/config/Ec2ComputeModule.java
+++ drivers/src/main/java/de/uniulm/omi/cloudiator/sword/drivers/ec2/config/Ec2ComputeModule.java
@@ -21,8 +21,11 @@ package de.uniulm.omi.cloudiator.sword.drivers.ec2.config;
 import com.google.inject.Injector;
 import de.uniulm.omi.cloudiator.common.OneWayConverter;
 import de.uniulm.omi.cloudiator.sword.api.domain.TemplateOptions;
+import de.uniulm.omi.cloudiator.sword.api.strategy.CreateVirtualMachineStrategy;
+import de.uniulm.omi.cloudiator.sword.core.strategy.DefaultGetStrategy;
 import de.uniulm.omi.cloudiator.sword.drivers.ec2.EC2JCloudsViewFactory;
 import de.uniulm.omi.cloudiator.sword.drivers.ec2.converters.TemplateOptionsToEc2TemplateOptions;
+import de.uniulm.omi.cloudiator.sword.drivers.ec2.strategy.Ec2CreateVirtualMachineStrategy;
 import de.uniulm.omi.cloudiator.sword.drivers.jclouds.JCloudsViewFactory;
 import de.uniulm.omi.cloudiator.sword.drivers.jclouds.config.JCloudsComputeModule;
 
@@ -34,6 +37,14 @@ public class Ec2ComputeModule extends JCloudsComputeModule {
     @Override protected JCloudsViewFactory overrideJCloudsViewFactory(Injector injector,
         JCloudsViewFactory originalFactory) {
         return injector.getInstance(EC2JCloudsViewFactory.class);
+    }
+
+    @Override
+    protected CreateVirtualMachineStrategy overrideCreateVirtualMachineStrategy(Injector injector,
+        CreateVirtualMachineStrategy original) {
+        //todo this is not optimal as it may overwrite the binding of the get strategy.
+        return new Ec2CreateVirtualMachineStrategy(original,
+            injector.getInstance(DefaultGetStrategy.DefaultVirtualMachineGetStrategy.class));
     }
 
     @Override
---
+++ drivers/src/main/java/de/uniulm/omi/cloudiator/sword/drivers/ec2/strategy/Ec2CreateVirtualMachineStrategy.java
@@ -0,0 +1,56 @@
+package de.uniulm.omi.cloudiator.sword.drivers.ec2.strategy;
+
+import com.github.rholder.retry.*;
+import de.uniulm.omi.cloudiator.sword.api.domain.VirtualMachine;
+import de.uniulm.omi.cloudiator.sword.api.domain.VirtualMachineTemplate;
+import de.uniulm.omi.cloudiator.sword.api.strategy.CreateVirtualMachineStrategy;
+import de.uniulm.omi.cloudiator.sword.api.strategy.GetStrategy;
+import de.uniulm.omi.cloudiator.sword.core.domain.VirtualMachineBuilder;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by daniel on 23.11.16.
+ */
+public class Ec2CreateVirtualMachineStrategy implements CreateVirtualMachineStrategy {
+
+    private final CreateVirtualMachineStrategy delegate;
+    private final GetStrategy<String, VirtualMachine> virtualMachineGetStrategy;
+
+    public Ec2CreateVirtualMachineStrategy(CreateVirtualMachineStrategy delegate,
+        GetStrategy<String, VirtualMachine> virtualMachineGetStrategy) {
+        this.delegate = delegate;
+        this.virtualMachineGetStrategy = virtualMachineGetStrategy;
+    }
+
+    @Override public VirtualMachine apply(VirtualMachineTemplate vmt) {
+        VirtualMachine virtualMachine = delegate.apply(vmt);
+
+        //sometimes virtual machines on amazon do not have a public ip at startup.
+        //we have to wait until we know it and then add it
+
+        //return early if ip is present
+        if (!virtualMachine.publicAddresses().isEmpty()) {
+            return virtualMachine;
+        }
+
+        Retryer<VirtualMachine> retryer = RetryerBuilder.<VirtualMachine>newBuilder()
+            .retryIfResult(input -> input.publicAddresses().isEmpty())
+            .withStopStrategy(StopStrategies.stopAfterDelay(2, TimeUnit.MINUTES))
+            .withWaitStrategy(WaitStrategies.fixedWait(10, TimeUnit.SECONDS)).build();
+
+        try {
+            VirtualMachine call =
+                retryer.call(() -> virtualMachineGetStrategy.get(virtualMachine.id()));
+            return VirtualMachineBuilder.of(virtualMachine)
+                .addPublicIpAddresses(call.publicAddresses()).build();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e.getCause());
+        } catch (RetryException e) {
+            //todo do we want to return the initial virtual machine here? probably yes
+            return virtualMachine;
+        }
+
+    }
+}
