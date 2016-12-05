4c69ea6f58261584a55dc364358d0024cfc85135
Daniel Baur <daniel.baur@uni-ulm.de>
2016-12-05 16:42:12 +0100

    PS-225 removed wildcard from set to avoid error in type inferring

--- drivers/openstack4j/src/main/java/de/uniulm/omi/cloudiator/sword/drivers/openstack4j/extensions/Openstack4JPublicIpService.java
+++ drivers/openstack4j/src/main/java/de/uniulm/omi/cloudiator/sword/drivers/openstack4j/extensions/Openstack4JPublicIpService.java
@@ -34,6 +34,7 @@ import org.openstack4j.model.compute.Server;
 
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.*;
@@ -73,8 +74,13 @@ public class Openstack4JPublicIpService implements PublicIpService {
 
     private String findPublicIp(ComputeFloatingIPService computeFloatingIPService,
         String virtualMachineId) throws PublicIpException {
-        final Set<FloatingIP> unassignedFloatingIps = computeFloatingIPService.list().stream()
-            .filter(floatingIP -> floatingIP.getInstanceId() == null).collect(Collectors.toSet());
+
+        final Set<? extends FloatingIP> unassignedFloatingIps =
+            computeFloatingIPService.list().stream().filter(new Predicate<FloatingIP>() {
+                @Override public boolean test(FloatingIP floatingIP) {
+                    return floatingIP.getInstanceId() == null;
+                }
+            }).collect(Collectors.toSet());
 
         if (unassignedFloatingIps.isEmpty()) {
             //no floating ip is present, allocate one from the pool.
