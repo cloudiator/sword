a388ecac703b5c2b0b71b1a14bbd5c9c9fe04b56
Daniel Baur <daniel.baur@uni-ulm.de>
2017-04-20 14:46:38 +0200

    Added os detection strategy interface

---
+++ core/src/main/java/de/uniulm/omi/cloudiator/sword/strategy/OperatingSystemDetectionStrategy.java
@@ -0,0 +1,13 @@
+package de.uniulm.omi.cloudiator.sword.strategy;
+
+import de.uniulm.omi.cloudiator.domain.OperatingSystem;
+import de.uniulm.omi.cloudiator.sword.domain.Image;
+
+/**
+ * Created by daniel on 20.04.17.
+ */
+public interface OperatingSystemDetectionStrategy {
+
+    OperatingSystem detectOperatingSystem(Image image);
+
+}
