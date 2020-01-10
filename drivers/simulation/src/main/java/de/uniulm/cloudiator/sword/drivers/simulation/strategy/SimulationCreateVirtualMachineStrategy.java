777e4df32bf9c1b74370940f8cd41c466001cf8b
Daniel Baur <daniel.baur@uni-ulm.de>
2020-01-10 12:57:51 +0100

    configure sleep for vms

--- drivers/simulation/src/main/java/de/uniulm/cloudiator/sword/drivers/simulation/strategy/SimulationCreateVirtualMachineStrategy.java
+++ drivers/simulation/src/main/java/de/uniulm/cloudiator/sword/drivers/simulation/strategy/SimulationCreateVirtualMachineStrategy.java
@@ -19,6 +19,7 @@
 package de.uniulm.cloudiator.sword.drivers.simulation.strategy;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static de.uniulm.cloudiator.sword.drivers.simulation.config.SimulationConstants.Properties.VM_STARTUP_TIME;
 
 import com.google.common.base.Supplier;
 import com.google.inject.Inject;
@@ -34,8 +35,13 @@ import de.uniulm.omi.cloudiator.sword.strategy.CreateVirtualMachineStrategy;
 import java.util.Set;
 import java.util.UUID;
 import javax.annotation.Nullable;
+import javax.inject.Named;
 
 public class SimulationCreateVirtualMachineStrategy implements CreateVirtualMachineStrategy {
+
+  @Inject(optional = true)
+  @Named(VM_STARTUP_TIME)
+  private Long vmStartupTime = null;
 
   private final SimulationModel simulationModel;
   private final Supplier<Set<Location>> locationSupplier;
@@ -63,7 +69,13 @@ public class SimulationCreateVirtualMachineStrategy implements CreateVirtualMach
     validateExists(virtualMachineTemplate);
     validateMatches(virtualMachineTemplate);
 
-    System.out.println(simulationModel.vmStartupTime());
+    if (vmStartupTime != null) {
+      try {
+        Thread.sleep(vmStartupTime * 1000);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    }
 
     final String id = UUID.randomUUID().toString();
 
